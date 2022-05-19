77e664726d5118493850774b068088b70f55ba2f
rigglet <39748939+rigglet@users.noreply.github.com>
2022-05-19 11:45:01 +0100

    "finished exercise"

--- arrayMethods.js
+++ arrayMethods.js
@@ -1,75 +1,77 @@
 function forEach(array, cb) {
   for (let i = 0; i < array.length; i++) {
-    cb(array[i], i, array)
+    cb(array[i], i, array);
   }
 }
 
 function map(array, cb) {
-  const newArray = []
+  let newArray = [];
   for (let i = 0; i < array.length; i++) {
-    newArray.push(cb(array[i], i, array))
+    newArray.push(cb(array[i], i, array));
   }
 
-  return newArray
+  return newArray;
 }
 
 function filter(array, cb) {
-  const newArray = []
+  let newArray = [];
   for (let i = 0; i < array.length; i++) {
-    const element = array[i]
-    if (cb(element, i, array)) newArray.push(element)
+    const element = array[i];
+    if (cb(element, i, array)) newArray.push(element);
   }
 
-  return newArray
+  return newArray;
 }
 
 function reduce(array, cb, initialValue) {
-  let currentValue = initialValue
+  let currentValue = initialValue;
   for (let i = 0; i < array.length; i++) {
-    const element = array[i]
+    const element = array[i];
     if (initialValue == null && i === 0) {
-      currentValue = element
+      currentValue = element;
     } else {
-      currentValue = cb(currentValue, element, i, array)
+      currentValue = cb(currentValue, element, i, array);
     }
   }
 
-  return currentValue
+  return currentValue;
 }
 
 function some(array, cb) {
   for (let i = 0; i < array.length; i++) {
-    if (cb(array[i], i, array)) return true
+    if (cb(array[i], i, array)) return true;
   }
 
-  return false
+  return false;
 }
 
 function every(array, cb) {
   for (let i = 0; i < array.length; i++) {
-    if (!cb(array[i], i, array)) return false
+    if (!cb(array[i], i, array)) return false;
   }
 
-  return true
+  return true;
 }
 
 function flat(array, depth = 1) {
-  const newArray = []
+  const newArray = [];
+
   for (let i = 0; i < array.length; i++) {
-    const element = array[i]
+    const element = array[i];
     if (Array.isArray(element) && depth > 0) {
-      newArray.push(...flat(element, depth - 1))
+      newArray.push(...flat(element, depth - 1));
     } else {
-      newArray.push(element)
+      newArray.push(element);
     }
   }
-  return newArray
+
+  return newArray;
 }
 
 function find(array, cb) {
   for (let i = 0; i < array.length; i++) {
-    const element = array[i]
-    if (cb(element, i, array)) return element
+    let element = array[i];
+    if (cb(element, i, array)) return element;
   }
 }
 
@@ -82,4 +84,4 @@ module.exports = {
   every,
   flat,
   find,
-}
+};
